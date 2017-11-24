bd2f2c758ea936b84dcfed3189625bbed2af9ed6
Kouch-Sato <redkouchred@gmail.com>
2017-11-24 13:44:01 +0900

    change tasks_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119053117) do
+ActiveRecord::Schema.define(version: 20171124043037) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "due_date"
+    t.integer "status"
+    t.string "label"
   end
 
 end
