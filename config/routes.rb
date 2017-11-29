3316dea3be0463f8657b4d9ae1c5a1d746b72449
Kouch-Sato <redkouchred@gmail.com>
2017-11-29 11:38:23 +0900

    create user :index :edit :new

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   get 'home/top'
 
-  resources :tasks, only: [:create, :destroy, :index, :update, :edit]
-  resources :users, only: [:create, :destroy, :index, :update, :edit]
+  resources :tasks
+  resources :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
