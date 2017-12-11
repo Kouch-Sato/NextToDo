c1086e5263e72d1a32efd739c1f54c9683b479de
Kouch-Sato <redkouchred@gmail.com>
2017-12-11 19:48:51 +0900

    set role validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
     validates :name, { presence: true, uniqueness: true }
     validates :email, { presence: true, uniqueness: true }
     validates :password, { presence: true }
+    validates :role, { presence: true }
 
     enum role: [:not_admin, :admin]
-    
+
     has_many :tasks, dependent: :delete_all
     mount_uploader :image, ImageUploader
 end
