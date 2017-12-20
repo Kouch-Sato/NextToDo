a4b7ce91fcbc4eb8fb78df9400f0259b2864d28b
Kouch-Sato <redkouchred@gmail.com>
2017-12-20 16:39:05 +0900

    jquery-turbolinks(bug)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
+//= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
